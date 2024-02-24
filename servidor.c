ba780e1a4460cac00e324b7025e61f68c4ee8d63
Mariojoestar33 <mariano.pena15@gmail.com>
2024-02-24 13:59:24 -0600

    Correcciones

--- servidor.c
+++ servidor.c
@@ -30,60 +30,52 @@ void send_list(int sockfd, struct sockaddr_in *cliaddr, socklen_t len) {
     sendto(sockfd, end_of_file, strlen(end_of_file), 0, (const struct sockaddr *)cliaddr, len);
 }
 
+
+//Funcion para leer un archivo y mandar su contenido por medio del socket
 void send_file_content(int sockfd, struct sockaddr_in *cliaddr, socklen_t len, const char *filename) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error al abrir el archivo");
         return;
     }
-    
     char buffer[MAXLINE];
     size_t bytes_read;
-    
     // Leer el contenido del archivo y enviarlo al cliente
     while ((bytes_read = fread(buffer, 1, MAXLINE, file)) > 0) {
         sendto(sockfd, buffer, bytes_read, 0, (const struct sockaddr *)cliaddr, len);
     }
-    
     // Enviar delimitador de final de archivo al cliente
     char *end_of_file = "EOF";
     sendto(sockfd, end_of_file, strlen(end_of_file), 0, (const struct sockaddr *)cliaddr, len);
-    
-    fclose(file);
+    fclose(file); //Cierre del archivo
 }
 
 int main() {
-
     int sockfd;
     struct sockaddr_in servaddr, cliaddr;
     char buffer[MAXLINE];
     const char *welcome_message = "¡Bienvenido al servidor UDP!\n-lista";
-
     // Crear un socket UDP
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("Fallo al crear el socket...");
         exit(EXIT_FAILURE);
     }
-
     // Inicializar la estructura del servidor
     memset(&servaddr, 0, sizeof(servaddr));
     memset(&cliaddr, 0, sizeof(cliaddr));
-
     // Configurar la información del servidor
     servaddr.sin_family = AF_INET; // IPv4
     servaddr.sin_addr.s_addr = INADDR_ANY; // Recibira paquetes de cualquier dirección IP de la máquina
     servaddr.sin_port = htons(PORT); //Número de puerto donde se recibirán paquetes
-
     // Vincular el socket con la dirección y el puerto
     if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("Error al vincular el socket");
         exit(EXIT_FAILURE);
     }
-
     printf("Socket creado y vinculado correctamente.\n");
 
-    // Escuchar en el socket y enviar un mensaje de bienvenida cuando alguien se conecta
+    // Escuchar en el socket y responder a los request
     while(1) {
     
         socklen_t len = sizeof(cliaddr);
@@ -93,10 +85,10 @@ int main() {
 
         // Verificar si el mensaje recibido es "lista"
         if (strcmp(buffer, "lista") == 0) {
-            // Enviar la lista de elementos al cliente
             send_list(sockfd, &cliaddr, len);
             printf("Lista de elementos enviada.\n");
             continue;
+        //Se verifica si la peticion es alguno de los archivos que se tienen en la lista
         } else if (strcmp(buffer, "Bohemian") == 0) {
             send_file_content(sockfd, &cliaddr, len, "Bohemian.txt");
             printf("Contenido de Bohemian.txt enviado.\n");
